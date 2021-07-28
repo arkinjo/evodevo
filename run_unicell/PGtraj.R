9a5357105fcfa5a2f0a06bdaf69c0cf97a163b77
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2021-07-28 10:48:20 +0800

    Bugfix
    Fixed a bug reversing direction of genome

--- run_unicell/PGtraj.R
+++ run_unicell/PGtraj.R
@@ -1,11 +1,11 @@
 
-trajs <- read.table("traj4.dat",header=TRUE)
+trajs <- read.table("traj6.dat",header=TRUE)
 fit <- trajs$Fitness
 dfit = diff(fit)
 plot(dfit,main="Change in fitness",xlab="Generation",type="l")
 abline(h=0,col="red")
 
-nancs <- read.table("gen4_nanc.dat",header=TRUE)
+nancs <- read.table("gen6_nanc.dat",header=TRUE)
 nanc <- nancs$Ancestors
 
 plot(nanc,xlab="Generation",ylim=c(0,1000),main="Shape of graph",type="l")
@@ -15,11 +15,11 @@ phat <- c()
 ghat <- c()
 
 for (i in c(1:200)){
-  filename <- paste("PGtraj4_",i,".dat",sep="")
+  filename <- paste("PGtraj6_",i,".dat",sep="")
   pg <- read.table(filename,header=TRUE)
   p <- pg$Phenotype
   g <- pg$Genotype
-  pdfout <- paste("PGtraj4_",i,".pdf",sep="")
+  pdfout <- paste("PGtraj6_",i,".pdf",sep="")
   pdf(pdfout)
   plot(g,p,xlim=c(-15,15),ylim=c(-2,2),xlab="genotype",ylab="phenotype")
   dev.off()
