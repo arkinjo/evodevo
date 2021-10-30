84d8dccf9ce99c6b3933c9c1fbf44bdbb7ca3f38
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2021-10-30 16:16:03 +0800

    R

--- analysis/PGtraj.R
+++ analysis/PGtraj.R
@@ -3,15 +3,15 @@ aphat <- c()
 nphat <- c()
 ghat <- c()
 
-pdf("upop20211007pg.pdf")
-for (i in c(1:200)){
-  filename <- paste("upop20211007pg_",i,".dat",sep="")
+pdf("upop20211030pg.pdf")
+for (i in c(1:1000)){
+  filename <- paste("upop20211030testpg_",i,".dat",sep="")
   pg <- read.table(filename,header=TRUE)
   ap <- pg$AncPhen
   np <- pg$NovPhen
   g <- pg$Genotype
   title <- paste("Generation",i,sep=" ")
-  plot(g,ap,xlim=c(-10,10),ylim=c(-2,2),xlab="genotype",ylab="phenotype",main=title,col="red")
+  plot(g,ap,xlim=c(-10,10),ylim=c(-3,3),xlab="genotype",ylab="phenotype",main=title,col="red")
   points(g,np,col="blue")
   abline(h=0)
   abline(v=0)
