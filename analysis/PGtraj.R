4213456dee0dee2a3fe5e283fcf74f559880c2aa
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2022-01-28 10:50:05 +0800

    Update PGtraj.R

--- analysis/PGtraj.R
+++ analysis/PGtraj.R
@@ -1,11 +1,20 @@
 
+refgen <- 20 #Reference generation
+maxgen <- 200 #Epoch length
+popseed <- 20220115
+refgens <- c(1,refgen,maxgen)
+
 aphat <- c()
 nphat <- c()
 ghat <- c()
+sdap <- c()
+sdnp <- c()
+sdg <- c()
 
-pdf("upop20220115testpg.pdf")
-for (i in c(1:200)){
-  filename <- paste("upop20220115testpg_",i,".dat",sep="")
+pdffilename <- paste("upop",popseed,"testpg.pdf",sep="")
+pdf(pdffilename)
+for (i in c(1:maxgen)){
+  filename <- paste("upop",popseed,"testpg_",i,".dat",sep="")
   pg <- read.table(filename,header=TRUE)
   ap <- pg$AncPhen
   np <- pg$NovPhen
@@ -15,16 +24,26 @@ for (i in c(1:200)){
   points(g,np,col="blue")
   abline(h=0)
   abline(v=0)
-  muap = mean(ap)
-  munp = mean(np)
-  mug = mean(g)
-  aphat = append(aphat,muap)
-  nphat = append(nphat,munp)
-  ghat = append(ghat,mug)
+  #muap = mean(ap)
+  #munp = mean(np)
+  #mug = mean(g)
+  aphat = append(aphat,mean(ap))
+  nphat = append(nphat,mean(np))
+  ghat = append(ghat,mean(g))
+  sdap = append(sdap,sd(ap))
+  sdnp = append(sdnp,sd(np))
+  sdg = append(sdg,sd(g))
 }
 dev.off()
 
-plot(nphat,ylim=c(-10,10),type="l",xlab="Generation",ylab="Projected Phenotype",main="Mean phenotype",col="blue")
-abline(h=0)
-points(aphat,type="l",col="red")
+#Summarize trajectory: error plots for initial, reference and final generation.
+plot(ghat,aphat,xlim=c(-20,20),ylim=c(-10,10),xlab="Genotype",ylab="Phenotype",main="Mean Projected Trajectory",col="red",type="l")
+points(ghat,aphat,col="red")
+arrows(ghat[refgens], aphat[refgens]-sdap[refgens], ghat[refgens], aphat[refgens]+sdap[refgens], length=0.05, angle=90, code=3, col="red") 
+arrows(ghat[refgens]-sdg[refgens], aphat[refgens], ghat[refgens]+sdg[refgens], aphat[refgens], length=0.05, angle=90, code=3, col="red") 
+lines(ghat,nphat,col="blue")
+points(ghat,nphat,col="blue")
+arrows(ghat[refgens], nphat[refgens]-sdnp[refgens], ghat[refgens], nphat[refgens]+sdnp[refgens], length=0.05, angle=90, code=3, col="blue") 
+arrows(ghat[refgens]-sdg[refgens], nphat[refgens], ghat[refgens]+sdg[refgens], nphat[refgens], length=0.05, angle=90, code=3, col="blue") 
+
 
