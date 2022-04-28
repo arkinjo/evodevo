c355c7bfcbc37dc2a00df7694135b6be0c20f3c1
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2022-04-28 10:10:41 +0800

    Now I know how to do SVD with gonum.

--- ccphenv/crosscov.go
+++ ccphenv/crosscov.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"log"
 	"math/rand"
 )
 
@@ -18,4 +19,26 @@ func main() {
 		}
 	}
 	fmt.Println(mat.Trace(a))
+
+	var svd mat.SVD
+	ok := svd.Factorize(a, mat.SVDFull)
+	if !ok {
+		log.Fatal("SVD failed.")
+	}
+	U := mat.NewDense(5, 5, nil)
+	V := mat.NewDense(5, 5, nil)
+	svd.UTo(U)
+	svd.VTo(V)
+	vals := svd.Values(nil)
+	fmt.Println("Values:", vals)
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j <= i; j++ {
+			u1 := V.ColView(i)
+			u2 := V.ColView(j)
+			dot := mat.Dot(u1, u2)
+			fmt.Printf("\t%e", dot)
+		}
+		fmt.Println()
+	}
 }
