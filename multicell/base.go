e8cf93907582fbee41cdae30e526ec5712f57fc5
Akira Kinjo <993982+arkinjo@users.noreply.github.com>
2023-01-17 11:09:03 +0800

    scale omegas

--- multicell/base.go
+++ multicell/base.go
@@ -178,6 +178,9 @@ func SetParams(s Settings) {
 		omega_p = 1.0 / math.Sqrt(DensityP*float64(ngenes)*(2-tauG))
 	}
 	if maxDevStep == 1 {
+		omega_f *= 10.0
+		omega_g *= 10.0
+		omega_h *= 10.0
 		omega_p *= 10.0 //Arbitrary factor to increase sensitivity of NoDev.
 	}
 
