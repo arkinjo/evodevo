369614c5ffdaa754e1e466793f1e2dbe4912d454
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2023-03-29 10:11:47 +0800

    Update lproj1.R
    Prettier plots

--- Rscripts/lproj1.R
+++ Rscripts/lproj1.R
@@ -8,31 +8,43 @@
 #df.ccaproj <- data.frame(id=c(1:1000))
 colvec <- c()
 
-for (layers in c("EFGHJP","E_G__P","_FGHJP","EFGH__")){
+for (layers in c("EFGHJP","E_G__P","_FGHJP")){
   modelname <- switch(layers, "EFGHJP"="Full", "_FGHJP"="NoCue", "E_G__P"="NoHier", "EFGH__"="NoDev", "__G___"="Null"  )
   
   for(epoch in c(1:20)){
-    modelXenvmode <- sprintf("%s_%s",modelname,envmode)
-    envcolor <- switch(envmode+1,"magenta","cyan")
+    #modelXenvmode <- sprintf("%s_%s",modelname,envmode)
+    #envcolor <- switch(envmode+1,"magenta","cyan")
     eGalilist <- c() #Initialize for each model
     #for (envmode in c(0,1)){
     #str.epoch <- sprintf("%02d",epoch)
-    afilename <- sprintf("%s_100_0.05_0_%02d.lproj1",layers,envmode,epoch)
+    afilename <- sprintf("%s_100_0.05_0_%02d.lproj1",layers,epoch)
     df.alproj1 <- read.table(afilename,header=FALSE)
     eproj_a <- df.alproj1$V2
     Gproj_a <- df.alproj1$V3
-    afilename <- sprintf("%s_100_0.05_1_%02d.lproj1",layers,envmode,epoch)
+    cor_a <- cor(eproj_a,Gproj_a)
+    #if(cor_a < 0.0){ #Flip
+    #  Gproj_a <- -Gproj_a
+    #}
+    
+    afilename <- sprintf("%s_100_0.05_1_%02d.lproj1",layers,epoch)
     df.nlproj1 <- read.table(afilename,header=FALSE)
     eproj_n <- df.nlproj1$V2
     Gproj_n <- df.nlproj1$V3
-    tiff("%s-ccaproj-%02d.tif",modelname,epoch)
+    cor_n <- cor(eproj_n,Gproj_n)
+    #if(cor_n < 0.0){ #Flip
+    #  Gproj_n <- -Gproj_n
+    #}
+    
+    tiff(sprintf("%s-ccaproj-%02d.tif",modelname,epoch),width=2250,height=2250,units="px",pointsize=12,res=300)
     plot(Gproj_n,eproj_n,col="cyan",xlab="Pheno-Geno projection",ylab="Pheno-Cue projection",main=modelname,
-         cex.lab=1.5,cex.main=2.0)
+         cex.lab=1.5,cex.main=2.0,xlim=c(-10,10),ylim=c(-10,10))
+    abline(h=0)
+    abline(v=0)
     points(Gproj_a,eproj_a,col="magenta")
-    legend("bottomright",legend=c("Ancestral","novel"),lty=1,col=c("magenta","cyan"),title="Environment")
+    legend("bottomright",legend=c(sprintf("Ancestral (cor = %.5f)",cor_a),sprintf("Novel (cor = %.5f)",cor_n)),
+                                  lty=1,col=c("magenta","cyan"),title="Environment")
     dev.off()
     #eGalilist <- append(abs(sum(evec*Gvec)),eGalilist) #Calculate absolute value of dot product
-    }
     #df.eGali[modelXenvmode] <- eGalilist
     #colvec <- append(colvec,envcolor)
   }
